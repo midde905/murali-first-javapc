61777a3290ef2fce786d45d42cc252a6908d67a9
developer1 <developer@gmail.com>
2022-04-27 07:20:29 +0530

    java project

---
+++ src/main/java/com/muralifirstjava/murali_first_javapc/App.java
@@ -0,0 +1,14 @@
+package com.muralifirstjava.murali_first_javapc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello murali !" );
+        System.out.println("hello krishna");
+    }
+}
